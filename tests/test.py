07714cec434a4de1604f0f2121fdd5a849ff5001
Bernard Kagimu <62263169+panbern12@users.noreply.github.com>
2020-08-15 19:30:48 +0300

    Add Test DownloadingEmailAttachment

--- tests/test.py
+++ tests/test.py
@@ -1,5 +1,24 @@
+import unittest,sys 
+sys.path.insert(1, 'c:/users/kagimub/desktop/IBRD Project')
 from main import ETL
 
-def DownloadingAttachemntTest(): 
-    a = ETL()
-    assert isinstance(a.DownloadingEmailAttachment('Mukiza Rayner', 'HELLO TEST MAIL'), str)
+
+class TestFactorial(unittest.TestCase):
+    """
+    Our basic test class
+    """
+
+    def test_fact(self):
+        """
+        The actual test.
+        Any method which starts with ``test_`` will considered as a test case.
+        """
+        etl = ETL()
+        path = etl.DownloadingEmailAttachment('Bernard Kagimu', 'HELLO TEST MAIL')
+        self.assertIsInstance(path, str)
+
+        
+
+
+if __name__ == '__main__':
+    unittest.main()
